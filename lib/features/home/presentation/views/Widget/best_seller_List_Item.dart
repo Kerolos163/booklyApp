6e58eb702e0ab758ace5768985843861a6386810
Kerolos163 <kokoessa951@gmail.com>
2023-05-30 03:28:53 +0300

    book rating

--- lib/features/home/presentation/views/Widget/best_seller_List_Item.dart
+++ lib/features/home/presentation/views/Widget/best_seller_List_Item.dart
@@ -1,31 +1,33 @@
-
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../constants.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
+import 'Book_rating.dart';
 
 BestSellerListViewItem(context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 30),
-      child: SizedBox(
-        height: 130,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.amber,
-                    image: const DecorationImage(
-                        image: AssetImage(AssetData.testimage)),
-                    borderRadius: BorderRadius.circular(8)),
-              ),
+  return Padding(
+    padding: const EdgeInsets.only(left: 30),
+    child: SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.amber,
+                  image: const DecorationImage(
+                      image: AssetImage(AssetData.testimage)),
+                  borderRadius: BorderRadius.circular(8)),
             ),
-            const SizedBox(
-              width: 30,
-            ),
-            Column(
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Expanded(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
@@ -52,13 +54,16 @@ BestSellerListViewItem(context) {
                   children: [
                     Text("19.99 €",
                         style: Styles.textStyle20
-                            .copyWith(fontWeight: FontWeight.bold))
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    const Spacer(),
+                    BookRating()
                   ],
                 )
               ],
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
--- lib/features/home/presentation/views/Widget/home_body.dart
+++ lib/features/home/presentation/views/Widget/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
