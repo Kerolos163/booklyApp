52126e924a84b29a86be5951ce4e84e013bd7628
Kerolos163 <kokoessa951@gmail.com>
2023-05-30 16:50:05 +0300

    book rating

--- lib/features/home/presentation/views/Widget/Book_rating.dart
+++ lib/features/home/presentation/views/Widget/Book_rating.dart
@@ -4,26 +4,29 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/styles.dart';
 
 BookRating() {
-  return Row(
-    children:  [
-      const Icon(
-        FontAwesomeIcons.solidStar,
-        color: Color(0xffFFDD4F),
-      ),
-      const SizedBox(
-        width: 6,
-      ),
-      const Text(
-        "4.8",
-        style: Styles.textStyle16,
-      ),
-      const SizedBox(
-        width: 9,
-      ),
-      Text(
-        "(2390)",
-        style: Styles.textStyle14.copyWith(color: const Color(0xff707070)),
-      ),
-    ],
+  return Padding(
+    padding: const EdgeInsets.only(right: 40),
+    child: Row(
+      children:  [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Color(0xffFFDD4F),
+        ),
+        const SizedBox(
+          width: 6,
+        ),
+        const Text(
+          "4.8",
+          style: Styles.textStyle16,
+        ),
+        const SizedBox(
+          width: 9,
+        ),
+        Text(
+          "(2390)",
+          style: Styles.textStyle14.copyWith(color: const Color(0xff707070)),
+        ),
+      ],
+    ),
   );
 }
