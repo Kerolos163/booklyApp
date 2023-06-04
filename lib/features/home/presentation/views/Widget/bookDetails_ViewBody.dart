9f86422157045d98821e9aede4ee4f2d1678c997
Kerolos163 <kokoessa951@gmail.com>
2023-06-04 05:57:07 +0300

    add title and subtitle

--- lib/features/home/presentation/views/Widget/bookDetails_ViewBody.dart
+++ lib/features/home/presentation/views/Widget/bookDetails_ViewBody.dart
@@ -1,10 +1,10 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'custom_BooK_Image_item.dart';
 import 'custom_Book_Details_AppBar.dart';
-
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -20,7 +20,25 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .18),
             child: CustomBookImage(context),
-          )
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
         ],
       ),
     );
