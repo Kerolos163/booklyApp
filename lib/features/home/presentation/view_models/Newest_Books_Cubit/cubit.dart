d4912b19ce74335623fe4909d884ca9663332331
Kerolos163 <kokoessa951@gmail.com>
2023-06-28 05:38:08 +0300

    newest state and cubit done

---
+++ lib/features/home/presentation/view_models/Newest_Books_Cubit/cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../Data/repos/home_repo.dart';
+import 'state.dart';
+
+class Newest_Books_Cubit extends Cubit<Newest_Books_State> {
+  Newest_Books_Cubit(this.homeRepo) : super(Newest_Books_initstate());
+  static Newest_Books_State get(context) => BlocProvider.of(context);
+  final HomeRepo homeRepo;
+  Future<void> FetchFeatureBooks() async {
+    emit(NewestBooksLoadingState());
+    var result = await homeRepo.FetchNewestBooks();
+    result.fold((Failures) {
+      emit(NewestBooksFailureState(error: Failures.errMessage));
+    }, (Books) {
+      emit(NewestBooksSuccessState(Books: Books));
+    });
+  }
+}
