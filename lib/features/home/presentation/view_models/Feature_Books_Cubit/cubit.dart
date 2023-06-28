e490bc3d4b5eec3fc66eeacb66f786549363b0e1
Kerolos163 <kokoessa951@gmail.com>
2023-06-28 04:41:35 +0300

    Create Fetch Feature Books

--- lib/features/home/presentation/view_models/Feature_Books_Cubit/cubit.dart
+++ lib/features/home/presentation/view_models/Feature_Books_Cubit/cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/presentation/view_models/Feature_Books_Cubit/state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../Data/repos/home_repo.dart';
+
 class Feature_Books_Cubit extends Cubit<Feature_Books_State> {
-  Feature_Books_Cubit() : super(Feature_Books_initstate());
+  Feature_Books_Cubit(this.homeRepo) : super(Feature_Books_initstate());
   static Feature_Books_Cubit get(context) => BlocProvider.of(context);
+  final HomeRepo homeRepo;
+  Future<void> FetchFeatureBooks() async {
+    emit(FeatureBooksLoadingState());
+    var result = await homeRepo.FetchFeatureBooks();
+    result.fold((Failures) {
+      emit(FeatureBooksFailureState(error: Failures.errMessage));
+    }, (Books) {
+      emit(FeatureBooksSuccessState(Books: Books));
+    });
+  }
 }
