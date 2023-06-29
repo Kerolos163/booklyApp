7f055c745c4b29eb57ad0f1a6859f6105ff46fe0
Kerolos163 <kokoessa951@gmail.com>
2023-06-29 17:00:57 +0300

    cached Network image

--- lib/features/home/presentation/views/Widget/custom_BooK_Image_item.dart
+++ lib/features/home/presentation/views/Widget/custom_BooK_Image_item.dart
@@ -1,14 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
 Widget CustomBookImage(context, {required String img}) {
-  return AspectRatio(
-    aspectRatio: 2.6 / 4,
-    child: Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage(img),fit: BoxFit.fill),
-          borderRadius: BorderRadius.circular(16)),
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(16),
+    child: AspectRatio(
+      aspectRatio: 2.6 / 4,
+      child: CachedNetworkImage(
+        imageUrl: img,
+        fit: BoxFit.fill,
+        placeholder: (context, url) =>
+            const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) => const Icon(
+            Icons.signal_wifi_statusbar_connected_no_internet_4_sharp),
+      ),
     ),
   );
 }
